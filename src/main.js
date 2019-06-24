63e6b2bfa25abcc2f2e5494b89b03d4f6a75a015
LuanSir <625322698@qq.com>
2019-06-24 21:20:09 +0800

    路由拦截器

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Element from 'element-ui'
 // 加载第三方包资源不需要写相对路径，直接包名/路径即可
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
-import 'nprogress/nprogress.css'
+// import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import nprogress from 'nprogress'
 
 Vue.use(Router)
 
