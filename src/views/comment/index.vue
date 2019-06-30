0b1ed0e2c9153dc8e112ce14536b734f9d899ede
LuanSir <625322698@qq.com>
2019-06-30 08:55:55 +0800

    评论页面完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -10,10 +10,13 @@
       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
+            <!-- disabled,禁用属性 -->
           <el-switch
+            :disabled="scope.row.changeLoading"
             v-model="scope.row.comment_status"
             active-color="#13ce66"
             inactive-color="#ff4949"
+            @click.native="handleChangeCommentStatus(scope.row)"
           ></el-switch>
         </template>
       </el-table-column>
@@ -41,7 +44,36 @@ export default {
           response_type: 'comment'
         }
       }).then(data => {
+        // 遍历返回的数据，给返回的数据添加属性，用于给按钮添加禁用使用
+        data.results.forEach(item => {
+          item.changeLoading = false
+        })
         this.articles = data.results
+      })
+    },
+    handleChangeCommentStatus (item) {
+      // console.log(item)
+      item.changeLoading = true // 禁用开关开启
+      this.$http({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: item.id.toString()
+        },
+        data: {
+          allow_comment: item.comment_status
+        }
+      }).then(() => {
+        console.log(123)
+        this.$message({
+          type: 'success',
+          message: `${item.comment_status ? '启用' : '关闭'}评论状态成功`
+        })
+        item.changeLoading = false // 禁用开关关闭
+      }).catch(err => {
+        console.log(err)
+        item.changeLoading = false
+        this.$message.error('修改评论状态失败')
       })
     }
   }
